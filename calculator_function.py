dd18a40b9b669efe02b11866806971df11f4d3de
Tejas C R <tejascrtejascr898@gmail.com>
2026-08-26 00:12:18 +0530

    Day 18 - Added calculator functions

---
+++ calculator_function.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition:", add(a, b))
+print("Subtraction:", subtract(a, b))
